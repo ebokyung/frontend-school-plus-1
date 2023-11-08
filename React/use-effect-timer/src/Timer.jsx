99c80d10a9d8a92e33c79a4ba53958b64f11e4ad
ebokyung <dlqhrud303@naver.com>
2023-11-08 21:32:06 +0900

    add : 타이머 숫자 포맷, if문 조건 추가

--- React/use-effect-timer/src/Timer.jsx
+++ React/use-effect-timer/src/Timer.jsx
@@ -14,25 +14,32 @@ function Timer() {
   
   useEffect(() => {
     let interval;
-    if(isTimerOn) { 
+    if(isTimerOn && time > 0) { 
       interval = setInterval(() => {
         setTime(prev => prev-1);
       }, 1000);
-    } else {
+    } else if(!isTimerOn && time !== 0 && interval){
       clearInterval(interval);
     }
-    if(time === 0){ // 0이 그려지고 나서 뜨게하는 방법?
+    if(time === 0){
       alert('시간이 종료되었습니다!')
       clearInterval(interval);
-      setIsTimerOn(false);
-      setTime(10);
+      handleReset();
     }
-    return () => clearInterval(interval);
+    return () => {
+      if(interval) clearInterval(interval);
+    }
   }, [isTimerOn, time]);
+
+  const formatTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  };
 
   return (
     <div>
-      <p>0:{time}</p>
+      <p>{formatTime()}</p>
       <button onClick={() => handleToggle()}>{isTimerOn ? '정지' : '시작'}</button>
       <button onClick={() => handleReset()}>리셋</button>
     </div>
