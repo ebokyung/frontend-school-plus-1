2319e74f5a76d7b9b982e496e50ccb121060eae8
ebokyung <dlqhrud303@naver.com>
2023-11-08 15:33:42 +0900

    init

---
+++ React/use-effect-timer-master/src/Timer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Timer() {
+  return (
+    <div>
+      <p>숫자</p>
+      <button>시작</button>
+      <button>리셋</button>
+    </div>
+  );
+}
+
+export default Timer;
